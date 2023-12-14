1afe6f432bf0df364dc9627cafa1dabd169061f6
alexander <alexandernyaga1999@gmail.com>
2023-12-14 08:59:56 +0300

    Plan trip

--- trip_planning.py
+++ trip_planning.py
@@ -1,1 +1,13 @@
 from database import Session, Trip, hiker
+
+def plan_trip():
+
+    session = Session()
+
+    #Create new trip
+    new_trip = Trip(name="Example Trip", destination="Mountain X", date="2023-12-31")
+    session.add(new_trip)
+
+
+    session.commit()
+    session.close()
