9a32fd74028a18cd613a1dc0808c61b0fbff80a4
alexander <alexandernyaga1999@gmail.com>
2023-12-14 11:24:40 +0300

    Add new equipment

--- equipment.py
+++ equipment.py
@@ -1,2 +1,14 @@
 from database import Session, equipment
 
+def manage_equipment():
+
+    session = Session()
+
+    new_equipment = equipment(name="Hiking Boots", condition="Good")
+
+    session.add(new_equipment)
+
+
+    
+
+    
--- trip_planning.py
+++ trip_planning.py
@@ -5,9 +5,10 @@ def plan_trip():
     session = Session()
 
     #Create new trip
-    new_trip = Trip(name="Example Trip", destination="Mountain X", date="2023-12-31")
+    new_trip = Trip(name="  Roadtrip", destination="Mt Kenya", date="2023-12-31")
     session.add(new_trip)
 
 
     session.commit()
     session.close()
+
