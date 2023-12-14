a3f1432c2796c18c1452d9f79aa784e6b9f47bcc
alexander <alexandernyaga1999@gmail.com>
2023-12-14 05:49:01 +0300

    Create association table

--- database.py
+++ database.py
@@ -2,4 +2,11 @@
 from sqlalchemy import create_engine, Column,Integer,String,ForeignKey,Table
 from sqlalchemy.orm import declarative_base,relationship,sessionmaker
 
+Base = declarative_base()
 
+hiker_trip_association = Table(
+    'hiker_trip_association', Base.metadata,
+    Column('hiker_id',Integer, ForeignKey('hiker.id')),
+    Column('trip_id', Integer,ForeignKey('trip.id'))
+    
+)
