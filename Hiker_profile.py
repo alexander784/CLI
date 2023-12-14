4c113ca1f58b8159d0ababf02b5b26ac7523a489
alexander <alexandernyaga1999@gmail.com>
2023-12-14 13:07:30 +0300

    Create a hikers profile

---
+++ Hiker_profile.py
@@ -0,0 +1,24 @@
+from database import Session, hiker
+
+def create_hiker_profile():
+
+    session = session()
+
+    name = input("Alex")
+    contact_info = input("07464")
+    emergency_contact = input("123")
+    preferences = input("great")
+
+    ##Create hew hiker profile
+
+    new_hiker = hiker(
+        name=name,
+        contact_info = contact_info,
+        emergency_contact=emergency_contact,
+        preferences=preferences
+    )
+
+    session.add(new_hiker)
+
+    session.commit()
+
